637e176ef56155030a44a3d74f5fdab847e0b5f8
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2022-07-07 10:38:20 +0000

    site: boostrap web site
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,7 @@
+import { defineUserConfig } from 'vuepress'
+
+export default defineUserConfig({
+  lang: 'en-US',
+  title: 'DSU Computer Scienze Tutoring',
+  description: 'TODO: add a description here',
+})
