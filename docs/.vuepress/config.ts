462368259bed590c5f3c21f8d807faa559817405
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2022-08-01 17:18:34 +0000

    add site description

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -5,8 +5,8 @@ import { hopeTheme } from "vuepress-theme-hope";
 export default defineUserConfig({
   lang: 'en-US',
   title: 'DSA Computer Scienze Tutoring',
-  description: 'TODO',
-  logo: 'https://study-eu.s3.amazonaws.com/uploads/university/university-of-pisa-logo.png',
+  description: 'Pagina dedicata al servizio di tutorato per studenti DSA del Dipartimento di Informatica.',
+  logo: '/images/university-of-pisa-logo.png',
   theme: hopeTheme({
     sidebar: false,
     navbar: [
